7456c5775ad63f4a9cf8ef8f09c0a98613853e55
hq2070 <hq2070@nyu.edu>
2023-12-03 21:10:23 -0500

    remove mongoURI

--- server/config/db.js
+++ server/config/db.js
@@ -2,12 +2,9 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config();
 mongoose.set("strictQuery", false);
-// MONGO_URI =
-//   "mongodb+srv://first-user:4sSBEFDzuDBXSrIN@cluster0.nbktbxi.mongodb.net/";
 
 mongoose
   .connect(process.env.MONGO_URI)
-  // .connect(MONGO_URI)
   .then(() => console.log("Connected to Database"))
   .catch((err) => console.error(err));
 
