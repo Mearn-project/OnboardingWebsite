27a410212d2311667a6cc6ccf16285aa1a801a0d
Kevin <mqfang211@gmail.com>
2023-12-07 17:24:18 -0500

    modified aws.js

--- server/utils/aws.js
+++ server/utils/aws.js
@@ -8,8 +8,8 @@ const AWS_ACCESS_KEY = process.env.AWS_ACCESS_KEY;
 const AWS_SECRET_KEY = process.env.AWS_SECRET_KEY;
 
 AWS.config.update({
-    accessKeyId: 'AKIAWFVLTXQIDTBG3U5M',
-    secretAccessKey: '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',
+    accessKeyId: AWS_ACCESS_KEY,
+    secretAccessKey: AWS_ACCESS_KEY,
     region: 'us-east-2'
   });
 
