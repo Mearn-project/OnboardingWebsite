1f5f022878e2ae39d2708dc7cd650f759a3512a6
hq2070 <hq2070@nyu.edu>
2023-12-08 18:02:07 -0500

    updat hr backend

--- server/models/User.js
+++ server/models/User.js
@@ -1,50 +1,49 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const refType = mongoose.Schema.Types.ObjectId;
 
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        require: true
-    },
-    email: {
-        type: String,
-        require: true
-    },
-    password: {
-        type: String,
-        require: true
-    },
-    isHR: {
-        type: Boolean,
-        default: false
-    },
-    application: {
-        type: refType,
-        ref: 'Application'
-    },
-    applicationStatus: {
-        type: String,
-        enum: ['Not Started', 'Pending', 'Rejected', 'Approved'],
-        default: 'Not Started'
-    },
-    visa: {
-        type: refType,
-        ref: 'Visa'
-    },
-    visaStatus: {
-        type: String,
-        enum: ["Approved", "Not Approved"],
-        default: "Not Approved",
-    },
-    housing: {
-        type: refType,
-        ref: 'House'
-    },
-    feedback: {
-        type: String
-    }
-
-})
+  username: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  isHR: {
+    type: Boolean,
+    default: false,
+  },
+  application: {
+    type: refType,
+    ref: "Application",
+  },
+  applicationStatus: {
+    type: String,
+    enum: ["Not Started", "Pending", "Rejected", "Approved"],
+    default: "Not Started",
+  },
+  visa: {
+    type: refType,
+    ref: "Visa",
+  },
+  visaStatus: {
+    type: String,
+    enum: ["Approved", "Not Approved", "US Citizen"],
+    default: "Not Approved",
+  },
+  housing: {
+    type: refType,
+    ref: "House",
+  },
+  feedback: {
+    type: String,
+  },
+});
 
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
