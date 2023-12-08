dcbacf35f94feab6a5c30bfc9a457062f939a9d1
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-12-07 21:27:01 -0500

    delete login component for user specific

--- frontend/src/app/components/employees/login/login.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'employee-login',
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.scss',
-})
-export class LoginComponent {}
