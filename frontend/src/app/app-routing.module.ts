a6296d205170826f6d724e43528fc0583adf81c8
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-12-09 11:08:43 -0500

    Update app-routing.module.ts
    - updated app-routing with employee routes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './share/login/login.component';
 
-
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'hr', loadChildren: () => import('./hr/hr.module').then(m => m.HrModule) },
-
+  {
+    path: 'hr',
+    loadChildren: () => import('./hr/hr.module').then((m) => m.HrModule),
+  },
+  {
+    path: 'employee',
+    loadChildren: () =>
+      import('./modules/employee/employee.module').then(
+        (m) => m.EmployeeModule
+      ),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
