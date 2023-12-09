fb7c157e88b3c86c9845ac5f0c1f4f083ed48a70
dzhuang725 <36779170+dzhuang725@users.noreply.github.com>
2023-12-08 21:45:04 -0500

    users can register
    - when user press the register button with correct input, the info is added to database

--- frontend/src/app/components/employees/registration/registration.component.ts
+++ frontend/src/app/components/employees/registration/registration.component.ts
@@ -1,8 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+// import { RegistrationService } from '../../../services/registration.service';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'employee-registration',
   templateUrl: './registration.component.html',
   styleUrl: './registration.component.scss',
 })
-export class RegistrationComponent {}
+export class RegistrationComponent implements OnInit {
+  registrationForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    // private registrationService: RegistrationService,
+    private http: HttpClient,
+    private router: Router
+  ) {
+    this.registrationForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPwd: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.registrationForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+      confirmPwd: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit(): void {
+    if (this.registrationForm.valid) {
+      console.log('Form is valid', this.registrationForm.value);
+      this.http
+        .post(
+          'http://localhost:3000/api/user/register/complete',
+          this.registrationForm.value
+        )
+        .subscribe({
+          next: (response) => {
+            console.log('User registered successfully:', response);
+          },
+          error: (error) => {
+            console.error('Error registering user:', error);
+          },
+        });
+      // this.registrationService.register(this.registrationForm.value).subscribe(
+      //   (success) => {
+      //     // TODO: Show message
+      //     console.log(success);
+      //     this.router.navigate(['/personal-information']);
+      //   },
+      //   (error) => {
+      //     console.error('Registration failed', error);
+      //   }
+      // );
+    }
+  }
+}
